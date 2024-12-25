7dedfa3482eaf4951202f71c4f2ceb79669963e3
koto74 <kotoganaiyo@gmail.com>
2024-12-25 12:09:33 +0900

    chore: タイトルつけた

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {ManageBoard} from './components/ManageBoard'
 export const App: React.FC = () => {
   return (
     <div>
-      <h1>Hexagon Grid</h1>
+      <h1>Hexagon Minesweeper</h1>
       <ManageBoard rows={10} columns={11} hexSize={30} />
     </div>
   )
